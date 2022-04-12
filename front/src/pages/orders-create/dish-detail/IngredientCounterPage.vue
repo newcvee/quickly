36a28894c959409c9a12124da3f86b638665fe1e
_cvee <cvelhninielkarti@gmail.com>
2022-04-12 22:19:37 +0200

    ingredients quitado

--- front/src/pages/orders-create/dish-detail/IngredientCounterPage.vue
+++ front/src/pages/orders-create/dish-detail/IngredientCounterPage.vue
@@ -1,24 +1,16 @@
 <template>
   <div>
-    <h1>{{dish.name}}hey</h1>
+    <h1>{{dish.name}}</h1>
     <!-- <img alt="foto" :src="dish.img" /> -->
     <p>{{dish.img}}</p>
   </div>
-  <section class="counter" v-for="ingredient in ingredients" :key="ingredient.dish_id">
-    <section class="buttons" >
-      <button @click="onMinusClicked">-</button>
-      <h2>{{ counter }}</h2>
-      <button @click="onAddClicked">+</button>
-    </section>
-    <p class="ingredient">{{ingredient.name}}</p>
-  </section>
+
   <button>Añande al carrito</button>
   {{$data}}
 </template>
 
 <script>
 import { getDishById } from "@/services/api.js";
-import { getIngredientByDish } from "@/services/api.js";
 
 export default {
   name: "Counter",
@@ -31,45 +23,12 @@ export default {
   },
   mounted() {
     this.loadData()
-    this.loadIngredients()
   },
   methods: {
-    onAddClicked() {
-      if(this.counter === "with"){
-        this.counter = "extra" 
-        this.$emit("value-changed", this.counter)
-      }
-      if(this.counter === "without"){
-        this.counter = "with" 
-        this.$emit("value-changed", this.counter)
-      }
-      
-     
-    },
-    onMinusClicked() {
-      if(this.counter=== "with"){
-        this.counter= "without"
-        this.$emit("value-changed", this.counter)
-      }
-      if(this.counter=== "extra"){
-        this.counter= "with"
-        this.$emit("value-changed", this.counter)
-      }
-      
-    },
     async loadData() {
       let DishId = this.$route.params.id;
       this.dish = await getDishById(DishId);
-    },
-    async loadIngredients() {
-      this.ingredients = await getIngredientByDish();
-      
-    },
-    CounterValue(){
-      let InitialValue= "with"
-      console.log(InitialValue)
     }
-
   },
 };
 </script>
--- front/src/services/api.js
+++ front/src/services/api.js
@@ -20,15 +20,6 @@ export async function getDish() {
     const dish = await response.json();
     return dish;
 }
-export async function getIngredientByDish() {
-    const settings = {
-      method: "GET",
-      
-    };
-    const response = await fetch(`${config.API_PATH}/dish/ingredients/1`, settings);
-    const ingredients = await response.json();
-    return ingredients;
-}
 
 export async function getDishesByCategory(category_id) {
   const settings = {
