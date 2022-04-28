eeec3c88745ada98779509828419eb4984854efb
_cvee <cvelhninielkarti@gmail.com>
2022-04-28 13:30:26 +0200

    *************ADMIN FRONT*****************

---
+++ Admin/src/pages/home/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <button @click="goToCategories">START ORDER</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  methods: {
+    goToCategories(){
+      this.$router.push("/categories")
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+h1 {
+  font-style: italic;
+}
+</style>
---
+++ Admin/src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/home/HomePage.vue'),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/pages/about/AboutPage.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: () => import('@/pages/orders-create/categories/CategoriesPage.vue')
+  },
+  {
+    path: '/category/dishes/:category_id',
+    name: 'Dishes',
+    component: () => import('@/pages/orders-create/dishes/DishesPage.vue')
+  },
+  {
+    path: '/dishes/:id',
+    name: 'OrdersCreate',
+    component: () => import('@/pages/orders-create/dish-detail/DishDetailPage.vue')
+  },
+  {
+    path: '/Thanks',
+    name: 'OrdersThanks',
+    component: () => import('@/pages/orders-create/dish-detail/ThanksPage.vue')
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
