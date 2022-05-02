6779aa16484cb5392ece4cb24c7594519ed41e7f
_cvee <cvelhninielkarti@gmail.com>
2022-05-02 13:40:13 +0200

    Add New Category form template created

---
+++ front-admin/src/pages/categories/AddCategoryPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h1>New Category</h1>
+    <form>
+      <label for="category-name">Give the new category a name:</label>
+      <input type="text">
+      <label for="category-id">Now, an id:</label>
+      <input type="text">
+      <label for="category-image">Lastly, an image:</label>
+      <input type="image">
+      <button>Submit</button>
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+form{
+  display: flex;
+  flex-direction: column;
+}
+button{
+  width: 10%;
+}
+
+</style>
