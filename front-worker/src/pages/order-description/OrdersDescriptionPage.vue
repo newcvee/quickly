5882d066da3f2b69b3cf81db224b737a7585d34b
_cvee <cvelhninielkarti@gmail.com>
2022-05-11 19:07:55 +0200

    order description page

--- front-worker/src/pages/order-description/OrdersDescriptionPage.vue
+++ front-worker/src/pages/order-description/OrdersDescriptionPage.vue
@@ -1,8 +1,8 @@
 <template>
 <main>
     <article v-for="order in orders" :key="order.order_id">
-    <p>{{order.order_status}}</p>
     <p>{{order.order_id}}</p>
+    <p>{{order.order_description}}</p>
     </article>
 </main>
   
@@ -35,7 +35,7 @@ export default {
 article {
     border: 1px solid black;
     margin: 0.5em 0.5em 0.5em;
-
+    
 }
 
 </style>
