f08e6505b5cbe5d382c9ed5bd48a9561374a13c4
_cvee <cvelhninielkarti@gmail.com>
2022-05-28 22:25:58 +0200

    price examples to integer

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -22,38 +22,38 @@ orders_repository = OrdersRepository(database_path)
 first_item= Item(id= "1",
 name = "burguer",
 img = "h7CEF3E4A60287C95",
-price = "10,95",
+price = 10.95,
 category_id = "2"
 )
 second_item= Item(id= "2",
 name = "milkshake",
 img = "h7CEF3E4A60287C95",
-price = "10,95",
+price = 10.95,
 category_id = "1"
 )
 
 third_item= Item(id= "3",
 name = "burguer + milkshake ",
 img = "h7CEF3E4A60287C95",
-price = "10,95",
+price = 10.95,
 category_id = "3"
 )
 fourth_item= Item(id= "4",
 name = "taco",
 img = "h7CEF3E456541",
-price = "10,95",
+price = 10.95,
 category_id = "2"
 )
 fifth_item= Item(id= "5",
 name = "chicken burrito",
 img = "h7CEF3E445645",
-price = "10,95",
+price = 10.95,
 category_id = "2"
 )
 sixth_item= Item(id= "6",
 name = "sandwitch",
 img = "h7CEF3E4789789",
-price = "10,95",
+price = 10.95,
 category_id = "2"
 )
 
