dc48a119574a48341b649471d971b6815ea7e45d
_cvee <cvelhninielkarti@gmail.com>
2022-05-29 19:47:52 +0200

    Homepage basic css

--- front-kiosk/src/pages/home/HomePage.vue
+++ front-kiosk/src/pages/home/HomePage.vue
@@ -19,7 +19,39 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-style: italic;
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap');
+.home{
+  background-image: url('https://wallpaperbat.com/img/263325-japanese-pc-wallpaper-top-free-japanese-pc-background.jpg');
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  font-family: "Roboto Mono", monospace;
+}
+button{
+  margin: 50px;
+  font-family: inherit;
+  font-size: 20px;
+  font-weight: 200;
+  color: rgb(22, 32, 67);
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid rgb(22, 32, 67);
+  cursor: pointer;
+  position: relative;
+  background-color: white;
+}
+button::after{
+  content: "";
+  background-color: rgb(22, 32, 67);
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 7px;
+  left: 7px;
 }
 </style>
