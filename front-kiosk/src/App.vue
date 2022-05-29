051f8bff1ddf4237afeb1e259a74199262831e33
_cvee <cvelhninielkarti@gmail.com>
2022-05-29 17:20:44 +0200

    link to categories in nav

--- front-kiosk/src/App.vue
+++ front-kiosk/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>|
-    <router-link to="/cart">Cart</router-link>
+    <router-link to="/categories">Categorias</router-link>|
+    <router-link to="/cart">Carrito</router-link>
   </div>
   <router-view />
 </template>
+
+<style scoped>
+#nav{
+  background-color: red;
+  height: 1vh;
+}
+</style>
