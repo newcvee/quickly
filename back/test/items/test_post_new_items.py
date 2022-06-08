0f9d61742cd69e52d42711d17cc93b3a67b23975
_cvee <cvelhninielkarti@gmail.com>
2022-06-08 14:30:07 +0200

    FIXED test post new item

--- back/test/items/test_post_new_items.py
+++ back/test/items/test_post_new_items.py
@@ -1,30 +1,31 @@
 from src.lib.utils import temp_file
 from src.webserver import create_app
-from src.domain.dish import DishRepository, Dish
+from src.domain.item import Item, ItemsRepository
 
-def test_front_should_post_new_dish():
-    dishes_repository = DishRepository(temp_file())
-    app = create_app(repositories={"dishes": dishes_repository})
+
+def test_front_should_post_new_item():
+    item_repository = ItemsRepository(temp_file())
+    app = create_app(repositories={"items": item_repository})
     client = app.test_client()
 
     body = {
-        "id" : "0015",
+        "id" : "15",
         "name" : "test menu ",
         "img" : "h7CEF3E4A60287C95",
-        "price" : "10,95",
+        "price" : 10.95,
         "category_id" : "3"   
     }
 
-    response_post = client.post("/api/dishes", json=body)
+    response_post = client.post("/api/items", json=body)
     assert response_post.status_code == 200
 
-    response_get = client.get("/api/dishes/0015")
+    response_get = client.get("/api/items/15")
     assert response_get.status_code == 200
     assert response_get.json == {
-        "id" : "0015",
+        "id" : "15",
         "name" : "test menu ",
         "img" : "h7CEF3E4A60287C95",
-        "price" : "10,95",
+        "price" : 10.95,
         "category_id" : "3"
     }
 
