a6251464e1d52268383827ee6e827c45f34d1e35
_cvee <cvelhninielkarti@gmail.com>
2022-06-12 14:34:45 +0200

    Pantalla principal flex organizacion

--- front-worker/src/pages/orders/OrdersHistoryPage.vue
+++ front-worker/src/pages/orders/OrdersHistoryPage.vue
@@ -1,9 +1,15 @@
 <template>
 <main>
+    <!-- <p>hello </p>
     <article v-for="order in orders" :key="order.order_id">
-    <p>{{order.order_status}}</p>
+    <p>{{order.order_state}}</p>
     <p>{{order.order_id}}</p>
-    </article>
+    </article> -->
+    <section class="changing-states">
+        <section class="waitlist-state"></section>
+        <section class="doing-state"></section>
+    </section>
+    <section class="done-state"></section>
 </main>
   
 </template>
@@ -31,11 +37,34 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 article {
     border: 1px solid black;
     margin: 0.5em 0.5em 0.5em;
-
 }
-
+main{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+.changing-states{
+    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+}
+.waitlist-state{
+    width: 50vw;
+    height: 50vh;
+    background-color: yellow;
+}
+.doing-state{
+    width: 50vw;
+    height: 50vh;
+    background-color: blue;
+}
+.done-state{
+    width: auto;
+    height: 35vh;
+    background-color: red;
+}
 </style>
